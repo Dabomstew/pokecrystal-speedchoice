8175825f9109c78910c28fe9b817332135949870
Vicendithas <vincent.killion@gmail.com>
2022-06-25 12:47:07 -0400

    Added option for no Surf Music (doesn't do anything yet)
    Added a new option to remove Surf Music. Verified that the nicknames option still works. The new Surf Music option currently doesn't do anything.

--- engine/menus/options/main_options_2.asm
+++ engine/menus/options/main_options_2.asm
@@ -3,12 +3,15 @@ MainOptionsP2String::
 	db "        :<LF>"
 	db "BIKE MUSIC<LF>"
 	db "        :<LF>"
+	db "SURF MUSIC<LF>"
+	db "        :<LF>"
 	db "GIVE NICKNAMES<LF>"
 	db "        :@"
 
 MainOptionsP2Pointers::
 	dw Options_ParkBallEffect
 	dw Options_BikeMusic
+	dw Options_SurfMusic
 	dw Options_Nicknames
 	dw Options_OptionsPage
 MainOptionsP2PointersEnd::
@@ -23,9 +26,14 @@ Options_BikeMusic:
 	lb bc, DISABLE_BIKE_MUSIC, 5
 	jp Options_InvertedOnOff
 
+Options_SurfMusic:
+	ld hl, DISABLE_SURF_MUSIC_ADDRESS
+	lb bc, DISABLE_SURF_MUSIC, 7
+	jp Options_InvertedOnOff
+
 Options_Nicknames:
 	ld hl, SKIP_NICKNAMING_ADDRESS
-	lb bc, SKIP_NICKNAMING, 7
+	lb bc, SKIP_NICKNAMING, 9
 	ld de, .Options
 	jp Options_TrueFalse
 
