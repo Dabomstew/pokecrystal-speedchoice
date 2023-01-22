c495d9ed22ead969ff6599f837834cdeaf129307
= <=>
2023-01-22 08:53:30 +0000

    Version

--- engine/menus/intro_permaoptions.asm
+++ engine/menus/intro_permaoptions.asm
@@ -1,5 +1,5 @@
 VersionNumberText::
-        db "v7.4.6@"
+        db "v7.4.7@"
 
 SpeedchoiceVersion:
 ; Speedchoice Major Version
@@ -9,7 +9,7 @@ db 7
 db 4
 
 ; Speedchoice Revision
-db 6
+db 7
 
 
 
