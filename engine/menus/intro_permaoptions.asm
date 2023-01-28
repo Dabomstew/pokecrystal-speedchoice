fa715e724ccc34bc6178e75f39217487fb23dbc7
= <=>
2023-01-28 11:20:45 +0000

    Increment version

--- engine/menus/intro_permaoptions.asm
+++ engine/menus/intro_permaoptions.asm
@@ -1,5 +1,5 @@
 VersionNumberText::
-        db "v7.4.8@"
+        db "v7.4.9@"
 
 SpeedchoiceVersion:
 ; Speedchoice Major Version
@@ -9,7 +9,7 @@ db 7
 db 4
 
 ; Speedchoice Revision
-db 8
+db 9
 
 
 
