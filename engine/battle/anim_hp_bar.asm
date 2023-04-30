f91eeb417f9d4289d579b48428f3a7d743211db4
PikalaxALT <pikalaxalt@users.noreply.github.com>
2023-04-30 12:31:49 -0400

    Fix visual glitch with animating HP bar

--- engine/battle/anim_hp_bar.asm
+++ engine/battle/anim_hp_bar.asm
@@ -143,50 +143,29 @@ ShortAnim_UpdateVariables:
 	ret
 
 LongAnim_UpdateVariables:
-.loop
-	ld hl, wCurHPAnimOldHP
-	ld a, [hli]
-	ld e, a
-	ld a, [hli]
-	ld d, a
-	ld a, e
+	ld hl, wCurHPBarPixels
+	ld a, [wNewHPBarPixels]
 	cp [hl]
-	jr nz, .next
-	inc hl
-	ld a, d
-	cp [hl]
-	jr nz, .next
+	jr nz, .not_finished
 	scf
 	ret
-
-.next
-	ld l, e
-	ld h, d
-	add hl, bc
-	ld a, l
-	ld [wCurHPAnimOldHP], a
-	ld a, h
-	ld [wCurHPAnimOldHP + 1], a
-	push hl
-	push de
-	push bc
-	ld hl, wCurHPAnimMaxHP
-	ld a, [hli]
-	ld e, a
-	ld a, [hli]
-	ld d, a
-	ld a, [hli]
-	ld c, a
-	ld a, [hli]
-	ld b, a
-	call ComputeHPBarPixels
-	ld a, e
-	pop bc
-	pop de
-	pop hl
-	ld hl, wCurHPBarPixels
+.not_finished:
+	ld a, c
+	add [hl]
+	ld [hli], a
 	cp [hl]
-	jr z, .loop
+	jr z, .clamp
+	call LongHPBar_CalcPixelFrame
+	and a
+	ret
+
+.clamp:
+	ld hl, wCurHPAnimNewHP + 1
+	ld a, [hld]
+	ld e, a
+	ld a, [hld]
+	ld [hl], e
+	dec hl
 	ld [hl], a
 	and a
 	ret
@@ -310,12 +289,14 @@ HPBarAnim_BGMapUpdate:
 	jr z, .skip_delay
 	cp $5
 	jr z, .skip_delay
+	; Copy attr
 	ld a, $2
 	ldh [hBGMapMode], a
 	ld a, c
 	ldh [hBGMapThird], a
 	call DelayFrame
 .skip_delay
+	; Copy tiles
 	ld a, $1
 	ldh [hBGMapMode], a
 	ld a, c
@@ -416,4 +397,32 @@ ShortHPBar_CalcPixelFrame:
 .return_max
 	ld a, [wCurHPAnimMaxHP]
 	ld [wCurHPAnimOldHP], a
+	ret
+
+LongHPBar_CalcPixelFrame:
+	ldh [hMultiplier], a
+	ld hl, wCurHPAnimMaxHP
+	ld a, [hli]
+	ldh [hMultiplicand + 2], a
+	ld a, [hl]
+	ldh [hMultiplicand + 1], a
+	xor a
+	ld [hMultiplicand], a
+	call Multiply
+	ld a, HP_BAR_LENGTH_PX
+	ldh [hDivisor], a
+	push bc
+	ld b, $4
+	call Divide
+	pop bc
+	ld hl, wCurHPAnimOldHP
+	ldh a, [hRemainder]
+	add a ; test bit 7, result in carry
+	ldh a, [hQuotient + 3]
+	adc $0
+	ld [hli], a
+	ldh a, [hQuotient + 2]
+	adc $0
+	ld [hl], a
+	and a
 	ret
